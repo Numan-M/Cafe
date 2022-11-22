f35537243c0809bc47257bef2aa91c672e022f73
Numan-M <Numan_mahmood@hotmail.co.uk>
2022-11-22 16:17:11 +0000

    Unit Tests

--- test_miniproject4.py
+++ test_miniproject4.py
@@ -2,9 +2,10 @@
 # Act
 # Assert
 
-from MiniProject4 import view_list, adding_to_list, renaming_list_via_index, remove_entry
+from MiniProject4 import view_list, adding_to_list, renaming_list_via_index, remove_entry, read_file, orders_menu_view,new_customer_order,update_status
 from unittest.mock import patch
-# 1. View
+
+# 1. ViewS
 def test_viewing_product_list_with_index():
     # Assemble
     a_list_of_dicts = [{"name":"Numan","surname":"Mahmood"},{"name":"Is","surname":"Testing"}]
@@ -87,8 +88,7 @@ def test_removing_entry_from_list(mock_print,mock_input):
     # Assert
     assert actual == expected
 
-# Test 2 
-
+# Test case: 2 
 @patch('builtins.input', side_effect = "3")
 @patch('builtins.print')
 def test_print_removing_entry_from_list(mock_print,mock_input):
@@ -99,10 +99,82 @@ def test_print_removing_entry_from_list(mock_print,mock_input):
     {"Name":"Joe","Number":"07931729312"},
     {"Name":"Murr","Number":"0797398213"},
     {"Name":"Q","Number":"08793982173"}]
-    
     # Act
     remove_entry(title_of_menu,a_list_of_dicts)
-    
     # Assert
     mock_print.assert_called_with('\nPerson no. 3 has been removed.')
     
+# 5. Read  CSV
+def test_read_from_csv():
+    # Assemble
+    empty_list_of_dicts = []
+    a_list_of_dicts = [{"test1":"a","test2":"b"},{"test1":"c","test2":"d"}]
+    # Act
+    read_file('test',empty_list_of_dicts)
+    expected = a_list_of_dicts
+    # Assert
+    assert empty_list_of_dicts == expected
+
+# 6. Order Menu View
+def test_orders_menu_view():
+    # Assemble
+    a_list_of_dicts = [{"customer_name":"abc","customer_address":"abc","customer_phone":"abc","courier":"abc","status":"abc","items":"abc"}]
+    # Act
+    actual = orders_menu_view(a_list_of_dicts)
+    expected = '''
+0.
+______________________________________________________________________
+Customer Name: abc
+Customer Address: abc
+Customer phonenumber: abc
+Courier: abc
+Order Status: abc
+Items: abc
+______________________________________________________________________'''
+    # Assert
+    assert actual == expected
+
+# 7. New order
+@patch('builtins.input', side_effect=['a','new','customer','1','','3'])
+def test_new_order(mock_input):
+    # Assemble
+    an_order_list = []
+    
+    # Act
+    actual = new_customer_order(an_order_list)
+    expected = [{
+    "customer_name": "a", 
+    "customer_address": "new",
+    "customer_phone": "customer",
+    "courier": 3,
+    "status": "Preparing",
+    "items": "1"
+    }]
+
+    # Assert
+    assert actual == expected
+
+# 8. Update status
+@patch('builtins.input', side_effect = ['0','3'])
+def test_updating_order_status(mock_input):
+    # Assemble
+    a_list = [{
+    "customer_name": "a", 
+    "customer_address": "new",
+    "customer_phone": "customer",
+    "courier": 3,
+    "status": "Preparing",
+    "items": "1"
+    }]
+    # Act
+    actual = update_status(a_list,('Preparing','Ready for collection','Out for delivery','Complete'))
+    expected = [{
+    "customer_name": "a", 
+    "customer_address": "new",
+    "customer_phone": "customer",
+    "courier": 3,
+    "status": "Complete",
+    "items": "1"
+    }]
+    # Assert
+    assert actual == expected
